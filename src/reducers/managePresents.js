a31375171162dcce40c120c49581b599b38bf7bd
theo <teddyhoover@gmail.com>
2021-04-27 16:03:50 -0500

    commit tree fce0aa0c510595825d8adfe74374a67759ea8ff3

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0}, action){
+    if (action.type == 'INCREASE') {
+        return Object.assign({}, state, {
+            numberOfPresents: state.numberOfPresents + 1
+        })
+    } else {
+        return state
+    }
 }
+
